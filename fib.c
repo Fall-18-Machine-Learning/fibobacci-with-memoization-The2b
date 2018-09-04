6aa49ef33f2a18f1b8a4f6292d324818167ec407
Thomas Lenz <thomas.lenz96@gmail.com>
2018-09-04 15:55:59 -0500

    GNU readline and GMP implementation

--- fib.c
+++ fib.c
@@ -2,11 +2,6 @@
  *
  */
 
-#include <stdlib.h>	// malloc, rand
-#include <stdio.h>	// printf
-#include <time.h>	// time
-#include <string.h>	// strcmp
-
 #include "fib.h"
 
 typedef int bool;
@@ -23,16 +18,25 @@ struct fibStruct* createFibStruct(size_t size) {
     sFib->size = size;
     
     // This is where we are storing the fibonacci values
-    // Allocate the correct number of bytes for the platform (8 per index for 64 bit, 4 for 32). GCC/MinGW will decide what that is.
-    sFib->fib = malloc(sizeof(unsigned long long int) * size);
+    // Allocate the structs' memory boxes
+    sFib->fib = malloc(sizeof(mpz_t) * size);
 
     return sFib;
 }
 
 // Release the memory associated with a fibStruct, recursively
 void destroyFibStruct(struct fibStruct* sFib) {
+    // Destroy all of the mp structures
+    for(int index = 0; index < sFib->size; index++) {
+	mpz_clear(sFib->fib[index]);
+    }
+
+    // Free the memory allocated for their pointers
     free(sFib->fib);
+
+    // And free the memory of the structs
     free(sFib);
+
     return;
 }
 
@@ -45,6 +49,7 @@ struct fibStruct* buildFibStruct(size_t size) {
 }
 
 // Populate a fibStruct
+// This uses GMP now, which is slower but allows for more fibonacci values.
 void populateFibStruct(struct fibStruct* sFib) {
     size_t size = sFib->size;
 
@@ -56,21 +61,21 @@ void populateFibStruct(struct fibStruct* sFib) {
 	goto size0;
     }
 
-    // If someone's dumb enough to put in 0, handle it
+    // If someone's silly enough to put in 0, handle it
     else if(size == 0) {
 	return;
     }
 
 size1:
-    // The ULL suffix indicates that the constant should be stored as an unsigned 64-bit int
-    sFib->fib[1] = 1ULL;
+    mpz_init_set_ui(sFib->fib[1],1);
     
 size0:
-    sFib->fib[0] = 0ULL;
+    mpz_init(sFib->fib[0]); // This sets the value of a to 0, so we don't need to do any re-assigning
 
     // This will automatically be skipped if the size is <= 2, so don't waste time checking with an if
     for(int index = 2; index < size; index++) {
-	sFib->fib[index] = sFib->fib[index - 1] + sFib->fib[index - 2];
+	mpz_init(sFib->fib[index]);
+	mpz_add(sFib->fib[index], sFib->fib[index - 1], sFib->fib[index - 2]);
     }
 
     return;
@@ -78,45 +83,102 @@ size0:
 
 // Get the index-th fibonacci value from a fibonacci structure
 // Has size protection
-unsigned long long int fibValue(struct fibStruct* sFib, size_t index) {
+char* fibValue(struct fibStruct* sFib, size_t index) {
     // Since we're using unsigned long long ints, this can be an error checker, since you really shouldn't need to check the first index, and there isn't any other value we can use
     if(index >= sFib->size) {
 	return NULL;
     }
 
     else {
-	return sFib->fib[index];
+	char* str = mpz_get_str(NULL,10,sFib->fib[index]);
+	return str;    
     }
 
 }
 
-int main() {
-    // Any higher and we overflow. This is a 64-bit unsigned int, so not much more we can do without far more complicated structures I don't want to deal with
-    const int VAL = 94;
-    char str[100];
+void outputFibValues(struct fibStruct* sFib) {
+    for(int index = 1; index < sFib->size; index++) {
+	printf("%d: %s\n",index,fibValue(sFib,index));
+    }
+}
 
-    // Create and populate our structure
-    struct fibStruct* sFib = buildFibStruct(VAL);
+int strToIntErrors() {
+    // Error handling
+    if(errno == ERANGE) {
+	printf("That value is too big for a 64-bit unsigned integer. Please input a smaller value\n");
+	return -1;
+    }
+    
+    else if(errno == EINVAL) {
+	printf("Invalid value. This input accepts positive integers only\n");
+	return -1;
+    }
+
+    return 0;
+}
+
+int main() {
+    unsigned long long int calcSize;
+
+start:
+    ;
+    // Error checker
+    int error = 0;
+    do {
+	// Figure out how many fibonacci numbers to calc from user
+	char* sizeStr = readline("How many fibonacci numbers would you like to calculate, including 0? ");
+
+	// If it's EOF, exit cleanly
+	if(sizeStr == NULL) {
+	    printf("\n");
+	    exit(0);
+	}
+	else if(strcmp(sizeStr,"") == 0) {
+	    error = -1;
+	    continue;
+	}
+	
+	// Try to convert the string to a usable value
+	calcSize = strtoull(sizeStr,NULL,10);
+
+	error = strToIntErrors();
+
+	if(calcSize == 0) {
+	    printf("Invalid value. This input accepts positive integers only\n");
+	    error = -1;
+	}
+    } while(error == -1);
+
+    struct fibStruct* sFib = buildFibStruct(calcSize);
+
+    //outputFibValues(sFib); // @DEBUG
 
     while(true) {
+	printf("Which fibonacci index would you like? Must be between 0 and %d: ",sFib->size - 1);
+	char* str = readline(NULL);
 
-	printf("Which fibonacci index would you like? Must be between 1 and %d: ",sFib->size - 1);
-	if(scanf("%s",&str) == EOF) {
+	// readline returns the empty string on a newline w/o data, and NULL on EOF
+	// Handle each situation appropriately
+	if(str == NULL) {
 	    printf("\n");
 	    destroyFibStruct(sFib);
 	    exit(0);
 	}
-	
-	int input = (int)strtol(str,NULL,10);
 
-	if(input > sFib->size - 1 || input == 0) {
-	    printf("Input value must be between 1 and %d\n",sFib->size - 1);
+	else if (strcmp(str,"") == 0) {
+	    continue;
+	}
+	
+	unsigned long long int input = strtoull(str,NULL,10);
+
+	if(input >= sFib->size) {
+	    printf("Input value must be between 0 and %d\n",sFib->size - 1);
 	    continue;
 	}
 
-	unsigned long long int fibVal = fibValue(sFib,input);
+	char* fibVal = fibValue(sFib,input);
 
-	printf("%llu\n",fibVal);
+	printf("%s\n",fibVal);
     }
 
     // Release the memory. If we were using this later on to cache the values, we would do this once the structure is no longer useful.
